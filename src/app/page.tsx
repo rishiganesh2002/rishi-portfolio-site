73d3e065a24ee365c57a278cce4042a0f706b759
Ravichandran, Rishi Ganesh <98856261+rishiganesh2002@users.noreply.github.com>
2025-08-19 14:46:30 -0500

    chore: unused assets + altering view angle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,24 @@
 import Experience from "../components/home/avatar/Experience";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { useTheme } from "../context/ThemeContext";
 
 export default function Home() {
+  const theme = useTheme();
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <Canvas
-        shadows
-        camera={{ position: [0, 2, 5], fov: 25 }}
-        style={{ width: "50vw", height: "50vh" }}
-      >
-        <color attach="background" args={["#121219"]} />
-        <Experience />
-        <OrbitControls enableZoom={false} />
-      </Canvas>
+    <div className="font-sans min-h-screen p-8 pb-20 sm:p-20">
+      <div className="flex flex-col items-center justify-center pt-12">
+        <Canvas
+          shadows
+          camera={{ position: [0, 2, 5], fov: 25 }}
+          style={{ width: "50vw", height: "50vh" }}
+        >
+          <color attach="background" args={[theme.backgroundColor]} />
+          <Experience />
+          <OrbitControls enableZoom={false} />
+        </Canvas>
+      </div>
     </div>
   );
 }
